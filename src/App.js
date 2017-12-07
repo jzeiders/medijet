a1d0540d25944b2fd481ee9470d2ac549dde24ba
John Zeiders <zeiders@usc.edu>
2017-12-07 00:26:39 -0800

    HashBang Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import thunk from "redux-thunk";
 import Reducer from "./reducers";
 import { Countries, Flights, Home, Regions } from "./screens";
